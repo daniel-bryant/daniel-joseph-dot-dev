d98ccbae43b77dc5b70958d96183440340cee2de
Daniel Bryant <bryant.daniel.j@gmail.com>
2024-08-23 17:11:04 -0700

    Add basic info and showcase

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,47 +2,48 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Daniel Joseph Bryant" },
+    { name: "description", content: "Software Engineer" },
   ];
 };
 
+import rails101 from "app/images/rails101.png";
+import hipcamp from "app/images/hipcamp.png";
+import checkr from "app/images/checkr.png";
+import usertesting from "app/images/usertesting.png";
+import github from "app/images/github-mark-white.png";
+
+const allYears = new Date().getFullYear() - 2009;
+const proYears = new Date().getFullYear() - 2012;
+
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="font-sans">
+      <div className="p-4 flex justify-end bg-japanese-violet">
+        <a href="https://github.com/daniel-bryant/daniel-joseph-dot-dev">
+          <img src={github} width="25" />
+        </a>
+      </div>
+      <div className="p-4">
+        <h1 className="text-3xl">Daniel Joseph Bryant</h1>
+        <p>
+          <a href="/resume.pdf" className="underline">Resume</a>{' • '}
+          <a href="tel:+13018210735" className="underline">301-821-0735</a>{' • '}
+          <a href="mailto:daniel@danieljoseph.dev" className="underline">daniel@danieljoseph.dev</a>
+        </p>
+        <p>
+          Software engineer, architect, lead, and rubyist with {proYears} years of
+          professional experience and {allYears} years building websites.
+        </p>
+        <h1 className="text-3xl mt-4">Showcase</h1>
+        <img src={hipcamp} />
+        <img src={checkr} />
+        <img src={usertesting} />
+        <img src={rails101} />
+      </div>
+      <div className="p-4 flex justify-center text-japanese-violet">
+        <p>Thanks for visiting 👋</p>
+      </div>
     </div>
   );
 }
